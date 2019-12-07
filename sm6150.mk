19609cb2445808bac58950154ac4778ce7c7ccf4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-07 22:31:24 +0100

    sm6150-common: Introduce Safailnet

--- sm6150.mk
+++ sm6150.mk
@@ -330,7 +330,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # RCS
 PRODUCT_PACKAGES += \
