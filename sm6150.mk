65797fda017efbad897dd3a24338d1f9a22c76ce
Pig <pig.priv@gmail.com>
2020-07-20 22:56:37 +0200

    sm6150-common: Build thermal v2.0
     * android.hardware.thermal@1.0::IThermal/default is deprecated in
     compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: I140be2a9307e8cbf475c35fb561dd675f6458c4d
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -393,9 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@2.0-service.sm6150
 
 # Touchscreen
 PRODUCT_PACKAGES += \
